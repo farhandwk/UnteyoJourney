5eca8d7ea830aa88ca85adc5b8754b0ffe9c29e1
HilmySA <hilmysaputra1.0@gmail.com>
2025-06-14 17:45:44 +0700

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,125 +1,101 @@
 import React, { useEffect, useState } from 'react';
-import logo from "../../assets/logo.png"
+import logo from "../../assets/logo.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import "./Header.css"
+import "./Header.css";
 
-function Header({scrollToSection}) {
-    const [isActive, setIsActive] = useState(false);
-    const [isScrolled, setIsScrolled] = useState(false);
+function Header({ scrollToSection }) {
+  const [isActive, setIsActive] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-    const onNav = () => {
-      setIsActive(!isActive);
+  const onNav = () => {
+    setIsActive(!isActive);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
     };
-  
 
-    useEffect(() => {
-        const handleScroll = () => {
-        setIsScrolled(!isScrolled)
-        };
-    
-        window.addEventListener("scroll", handleScroll);
-    
-        return () => {
-          window.removeEventListener("scroll", handleScroll);
-        };
-      }, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div>
-      <div className={`containerHeader ${isScrolled ? "scrolled" : ""}
-      flex flex-row
-      w-screen h-[75px]
-      justify-between
-      pt-[20px]
-      pl-[20px]
-      pr-[20px]
-      bg-none
-      fixed top-[0]
-      z-999
-      
-      md:h-[120px]
-      md:pt-[30px]
-      md:pl-[30px]
-      md:pr-[30px]
+      {/* Header Container */}
+      <div className={`
+        containerHeader 
+        ${isScrolled ? "scrolled" : ""}
+        flex flex-row justify-between
+        w-screen h-[75px] pt-[20px] pl-[20px] pr-[20px]
+        bg-none fixed top-0 z-999
 
-      lg:flex-row-reverse
-      lg:h-[120px]
-      lg:pt-[10px]
-      lg:pl-[150px] lg:pr-[50px]
-      `}
-      >
-            <FontAwesomeIcon icon={faBars} 
+        md:h-[100px] md:pt-[30px] md:pl-[30px] md:pr-[30px]
+        lg:flex-row-reverse lg:h-[120px]
+        lg:pt-[10px] lg:pl-[150px] lg:pr-[50px]
+      `}>
+        {/* Hamburger Icon | karena pakai fontawesome jadi ukurannya pakai text size*/}
+        <FontAwesomeIcon 
+          icon={faBars}
           onClick={onNav}
           className="
-          onHamburger
-          text-[white]
-          w-[26px]
-          relative
-          top-[-8px]
-
-          md:w-[48px]
-          md:flex
-          md:top-[-10px]
-
-          lg:hidden
-          " />
-          
-          <ul
-          className="
-          lgHeader
-          hidden
-          Helvetica
-          text-[white]
-          transition-[0.1s]
-          cursor-[pointer]
-
-          lg:flex lg:flex-row
-          lg:gap-[70px]
-          lg:p-[30px]
-          lg:text-[18px]
+            onHamburger
+            text-white text-[26px] relative top-[3px]
+            md:text-[36px] md:flex md:top-[-3px]
           "
-          >
-              <li onClick={() => scrollToSection("aboutUs")}>About Us</li>
-              <li onClick={() => scrollToSection("program")}>Program</li>
-              <li onClick={() => scrollToSection("post")}>Post</li>
-              <li onClick={() => scrollToSection("event")}>Event</li>
-              <li onClick={() => scrollToSection("connect")}>Contact Us</li>
-          </ul>
-          <img src={logo}
-          className="
-          w-[73.98px] h-[80px]
-          relative
-          top-[-20px]
+        />
 
-          md:w-[110.976px] md:h-[120px]
-          md:top-[-10px]
-          "></img>
+        {/* Desktop Menu */}
+        <ul className="
+          lgHeader hidden lg:flex lg:flex-row
+          Helvetica text-white text-[18px]
+          transition-[0.1s] cursor-pointer
+          lg:gap-[70px] lg:p-[30px]
+        ">
+          <li onClick={() => scrollToSection("aboutUs")}>About Us</li>
+          <li onClick={() => scrollToSection("program")}>Program</li>
+          <li onClick={() => scrollToSection("post")}>Post</li>
+          <li onClick={() => scrollToSection("event")}>Event</li>
+          <li onClick={() => scrollToSection("connect")}>Contact Us</li>
+        </ul>
+
+        {/* Logo */}
+        <img 
+          src={logo}
+          className="
+            w-[73.98px] h-[80px] relative top-[-22px]
+            md:w-[110.976px] md:h-[120px] md:top-[-40px]
+            lg:w-[110.976px] lg:h-[120px] lg:top-[-13px]
+          "
+          alt="Logo"
+        />
       </div>
-      <div className={`Navbar ${isActive ? 'active' : ''} fixed
-        top-[0px] left-[0px]
+
+      {/* Mobile Navbar */}
+      <div className={`
+        Navbar ${isActive ? 'active' : ''}
+        fixed top-0 left-0 z-50
         flex flex-col lg:hidden
-        z-50`}>
-            <ul className="
-            NavbarMobile
-            HelveticaBold
-            text-[white] text-[18px]
-            w-screen
-            bg-[black]
-            flex flex-col
-            p-[25px]
-            pt-[75px]
-            gap-[15px]
-            ">
-              <li onClick={() => scrollToSection("aboutUs")}>About Us</li>
-              <li onClick={() => scrollToSection("program")}>Program</li>
-              <li onClick={() => scrollToSection("post")}>Post</li>
-              <li onClick={() => scrollToSection("event")}>Event</li>
-              <li onClick={() => scrollToSection("connect")}>Contact Us</li>
-            </ul>
+      `}>
+        <ul className="
+          NavbarMobile
+          HelveticaBold text-white text-[18px]
+          w-screen bg-black
+          flex flex-col
+          p-[25px] pt-[75px] gap-[15px]
+
+          md:pt-[100px]
+        ">
+          <li onClick={() => scrollToSection("aboutUs")}>About Us</li>
+          <li onClick={() => scrollToSection("program")}>Program</li>
+          <li onClick={() => scrollToSection("post")}>Post</li>
+          <li onClick={() => scrollToSection("event")}>Event</li>
+          <li onClick={() => scrollToSection("connect")}>Contact Us</li>
+        </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
