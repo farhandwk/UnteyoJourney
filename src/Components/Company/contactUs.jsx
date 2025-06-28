b6e725bc7c7131b2d03d46d790910875fc5d05c0
Yodha Ardiansyah <yodhaardiansyah30@gmail.com>
2025-06-28 17:48:19 +0700

    update contactUS

--- src/Components/Company/contactUs.jsx
+++ src/Components/Company/contactUs.jsx
@@ -9,30 +9,27 @@ function ContactUs() {
       <div className="max-w-6xl mx-auto px-6 py-16 space-y-12">
         {/* Section: Contact Us Title */}
         <div className="text-center space-y-4">
-          <h1 className="text-4xl font-bold ">Contact Us</h1>
-          <h3 className="text-3xl font-semibold pt-20">Get In Touch!!</h3>
-          <p className="text-xl md:text-base max-w-2xl mx-auto text-gray-300">
-            #BeWise
+          <h1 className="text-[40px] lg:text-[40px] font-bold">Contact Us</h1>
+          <h3 className="text-[25px] lg:text-[25px] font-semibold pt-12.5">Get In Touch!!</h3>
+          <p className="text-[20px] lg:text-[20px]  text-gray-300">
+            We are building a dynamic community of forward-thinkers and problem-solvers, and we would love to hear from you. Whether you have a question about our programs , an idea for collaboration , or feedback to share, our team is ready to assist. Your voice is crucial in helping us grow and improve our platform. Let's connect and shape the future of student empowerment together
           </p>
         </div>
 
         {/* Section: How Can We Assist You? */}
-        <div className="space-y-6 pt-20 text-center flex flex-col items-center">
-        <h3 className="text-3xl font-bold">How Can We Assist You?</h3>
+        <div className="space-y-6 pt-25 text-center flex flex-col items-center">
+        <h3 className="text-[25px] lg:text-[25px] font-semibold">How Can We Assist You?</h3>
         
-        <ul className="list-disc text-left pl-6 space-y-2 text-gray-300 text-sm md:text-base max-w-xl">
-            <li><strong>Pertanyaan Umum</strong>: Jika Anda memiliki pertanyaan tentang program ini, cara mendaftar, atau informasi umum lainnya.</li>
-            <li><strong>Dukungan Teknis</strong>: Untuk masalah terkait penggunaan platform atau kendala teknis lainnya.</li>
-            <li><strong>Kemitraan & Kolaborasi</strong>: Jika Anda adalah organisasi, institusi pendidikan, atau individu yang tertarik untuk berkolaborasi dengan Unteyo Journey.</li>
-            <li><strong>Saran & Masukan</strong>: Kami selalu terbuka untuk ide dan masukan Anda untuk terus meningkatkan layanan kami.</li>
-            <li><strong>Media & Pers</strong>: Untuk pertanyaan terkait media, wawancara, atau siaran pers.</li>
+        <ul className="list-disc text-left pl-6 space-y-2 text-gray-300 text-[20px] lg:text-[20px]  md:text-base ">
+            <li><strong>Partnerships & In-depth Inquiries</strong>: For inquiries regarding partnerships & collaboration , media & press , or to provide comprehensive suggestions & feedback, please reach out to us using the email address listed below. Our team will review your message and respond accordingly.</li>
+            <li><strong>Quick Questions & Support</strong>: Have a general question about our programs  or need technical support with our platform? "Min Un" is here to help. For a faster response, please click and connect with us through the "Min Un" WhatsApp contact provided below.</li>
         </ul>
         </div>
 
 
         {/* Section: Contact Information */}
-        <div className="space-y-4 pt-20">
-          <h3 className="text-3xl font-bold text-center">Contact Information</h3>
+        <div className="space-y-4 pt-25">
+          <h3 className="text-[25px] lg:text-[25px] font-semibold text-center">Contact Information</h3>
           <div 
             className="flex flex-row  
                 justify-center 
@@ -73,21 +70,22 @@ function ContactUs() {
             </div>
             <div className="text-center space-y-2">
                 <img
-                    src="/src/assets/wa-icon.png"
-                    alt="whatsapp"
+                    src="/src/assets/instagram-icon.png"
+                    alt="instagram icon"
                     className="
                         w-20
                         md:w-30
-                        lg:w-40 
+                        lg:m-5
+                        lg:w-30 
                         h-auto 
                         mx-auto 
                         transition-transform 
                         duration-300 
                         hover:scale-110"
                 />
-                <p>Min Un</p>
+                <p>@unteyojourney</p>
                  <a
-                     href="https://api.whatsapp.com/send/?phone=6287832163618&text=Hallo%20Admin&type=phone_number&app_absent=0"
+                     href="https://www.instagram.com/unteyojourney/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="bg-white text-black px-4 py-2 rounded-full text-sm font-bold inline-block hover:bg-gray-200 hover:scale-105 transition duration-300"
@@ -100,11 +98,14 @@ function ContactUs() {
         </div>
 
         {/* Section: Visit Our Hub */}
-        <div className="space-y-4 pt-20">
-          <h3 className="text-3xl font-bold text-center">Visit Our Hub</h3>
+        <div className="space-y-4 pt-25 pb-20">
+          <h3 className="text-[25px] lg:text-[25px] font-semibold text-center">Visit Our Hub</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div className="bg-gray-800 p-6 text-center flex items-center justify-center">
-              <p className="text-lg font-medium">Informasi Alamat<br />Kantor Kita</p>
+              <p className="text-lg font-medium">
+                <strong>UTY Creative HUB</strong>
+                <br/>
+                Jl. Siliwangi Jl. Jombor Lor, Mlati Krajan, Sendangadi, Kec. Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55284</p>
             </div>
             <div>
               <img src="/src/assets/uch-room.png" alt="Meeting Room" className="w-full rounded-md" />
